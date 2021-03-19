2ab0c81da05cc76ce80adee3012956581af1befe
TristanMcDonald <tristan.cole.mcdonald@gmail.com>
2021-03-19 14:17:10 +0200

    Initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+
+
+    public static void main(String[] args) {
+        calculatorGui calcGui = new calculatorGui();
+        calcGui.setVisible(true);
+    }
+    
+}
